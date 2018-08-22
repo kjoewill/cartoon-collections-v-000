43039ef637fe90a7c35bb5da4ed6b85c8471927e
Kevin W <kjoewill@gmail.com>
2018-08-22 17:07:21 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  sorted_names = names.sort
+  i = 0
+  while i < names
 end
 
 def summon_captain_planet# code an argument here
