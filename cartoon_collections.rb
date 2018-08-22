cf36e00a36a5b21d93f9230e98ca5abb051db298
Kevin W <kjoewill@gmail.com>
2018-08-22 17:33:16 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,9 +10,11 @@ def long_planeteer_calls(calls)
   calls.any? { |name| name.size > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  snacks
+  
 end
 
 dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
